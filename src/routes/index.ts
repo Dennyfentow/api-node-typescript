64ace50689fc9e1f431ab98b701f0881d8f5a420
Daniel dos Santos <daniel@automastersat.com.br>
2021-12-14 09:12:19 -0300

    atualizado para utilizar o types do RequestBody

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -17,9 +17,9 @@ router.get('/', (req, res) => {
 router.post('/', (req, res) => {
     const body = req.body as RequestBody;
     const newCustomer : Customer = {
-        id: req.body.id,
-        name: req.body.name,
-        birthDate: req.body.birthDate
+        id: body.id,
+        name: body.name,
+        birthDate: body.birthDate
     };
     customers.push(newCustomer);
     res.status(201).json(newCustomer);
